fcbdbde558764a41b0e5a71236597a0f00c69825
crizzelalexandra <alegarcia150907@gmail.com>
2025-08-21 13:19:33 -0400

    arreglo del logo,la juli me ayudo un poco

--- src/script.js
+++ src/script.js
@@ -2,3 +2,32 @@ document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
     alert("¡Solicitud enviada con éxito!");
 });
+const slides = document.querySelectorAll('.slide');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let index = 0;
+
+function mostrarSlide(n) {
+    if (n >= slides.length) index = 0;
+    if (n < 0) index = slides.length - 1;
+
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+prev.addEventListener('click', () => {
+    index--;
+    mostrarSlide(index);
+});
+
+next.addEventListener('click', () => {
+    index++;
+    mostrarSlide(index);
+});
+
+// Cambio automático cada 4 segundos
+setInterval(() => {
+    index++;
+    mostrarSlide(index);
+}, 4000);
